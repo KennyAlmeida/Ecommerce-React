fae24a7748fea97d3203b0abf7d7f3add0755199
KennyAlmeida <kenny_almeida_25@hotmail.com>
2023-02-07 19:08:27 -0300

    ItemCount.js

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import { Fragment } from 'react';
 import NavBar from './components/header/NavBar.js';
 import ItemListContainer from './components/ItemListContainer.js';
+import ItemCount from './components/ItemCount.js';
 
 function App() {
   return (
     <>
       <NavBar/>
       <ItemListContainer titulo="Welcome to store"/>
+      <ItemCount stock="5" initial="1"/>
+      
     </>
   );
 }
---
+++ ecommerce/src/components/ItemCount.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+function ItemCount({ stock, initial, onAdd }) {
+  const [count, setCount] = useState(initial)
+  
+
+  const handleAdd = () => {
+    if (count < stock) {
+      setCount(Number(count) + 1)
+    }
+  }
+
+  const handleRemove = () => {
+    if (count > 1) {
+      setCount(count - 1)
+    }
+  }
+
+  return (
+    <div className="itemCount">
+      <div className="itemCount__counter">
+        <button onClick={handleRemove}>-</button>
+        <span>{count}</span>
+        <button onClick={handleAdd}>+</button><br/>
+      </div>
+      <button onClick={() => onAdd(count)}>add to cart</button>
+    </div>
+  )
+}
+
+export default ItemCount
